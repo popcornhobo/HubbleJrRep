fbbcecca77bbf51f3ada55504bac706d6bc20b72
zacharysfrazee <zach@trailsendrepair.com>
2016-03-12 21:33:25 -0700

    Improved SPI Functionality
    - Removed unnecessary delays to improve speed
    - Added a define option to use NOP based delays
    - Added options to vary delay times for clocking and data sampling
    - Added define option to sample the signal for the VN-100 up to n times
    and vote on the most popular result.
    - Added Unit Test for testing code and the IMU
    - Added Unit Test to makefile
    - Tested speed with VN-100 library on DE0 without IMU. Time to complete
    one read of VN100_SPI_GetQuatRates() 1074us, 1460us with 3 samples.

---
+++ VN_100/VN_UnitTest.c
@@ -0,0 +1,119 @@
+/* VN_UnitTest.c
+ * 
+ * Unit test for VN-100 and SPI code
+ *
+ * Authors:	Zach Frazee
+ * 
+ *
+ * Revions:
+ * 2016-3-12	v0.1	Initial Revison
+ *
+ * */
+ 
+/*----------------------------------------------------------------------*/
+/*	SYSTEM DEFINES */
+
+#define soc_cv_av
+#define HW_REGS_BASE ( ALT_STM_OFST )
+#define HW_REGS_SPAN ( 0x04000000 )
+#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
+
+
+
+/*----------------------------------------------------------------------*/
+/*	SYSTEM INCLUDES */
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "hwlib.h"
+#include "soc_cv_av/socal/socal.h"
+#include "soc_cv_av/socal/hps.h"
+#include "soc_cv_av/socal/alt_gpio.h"
+#include "hps_0.h"
+
+
+
+/*-----------------------------------------------------------------------*/
+/*	USER INCLUDES */
+
+#include "soft_spi.h"
+#include "VN_lib.h"
+
+
+
+/*-----------------------------------------------------------------------*/
+/*	MAIN */
+
+int main() {
+	
+	
+	/*INITIALIZE MEMORY MAPPING	*/
+	void *virtual_base;
+	int fd;	
+	
+	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+		printf( "ERROR: could not open \"/dev/mem\"...\n" );
+		return( 1 );
+	}
+
+	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
+	
+	if( virtual_base == MAP_FAILED ) {
+		printf( "ERROR: mmap() failed...\n" );
+		close( fd );
+		return( 1 );
+	}
+	
+	
+	/* INITIALIZE SPI */
+	SPI_Init(virtual_base);
+	
+	
+	/* MAIN LOOP */
+	printf("Starting...\n");
+	for(;;){
+		
+		
+		/* SETUP TIMER */
+		struct  timeval start_time, end_time;
+		
+		
+		/* Read Data From VN-100 */
+		printf("Getting Quarternion Rates from VN-100\n");
+		
+		unsigned char sensorID = 0;
+		float q[4];
+		float rates[3];
+		
+		gettimeofday(&start_time, NULL); //Starting time of write
+		
+		VN100_SPI_Packet * ReturnPacket = VN100_SPI_GetQuatRates(sensorID, q, rates);
+		
+		gettimeofday(&end_time, NULL); //Ending time of write
+		
+		printf("q0: %f\t q1: %f\t q2: %f\t q3: %f\n", q[0], q[1], q[2], q[3]);
+		
+		printf("rate0: %f\t rate1: %f\t rate2: %f\n", rates[0], rates[1], rates[2]);
+		
+		printf("Error ID: %u\n", *(uint8_t*)ReturnPacket);
+		
+		unsigned long int time = (end_time.tv_sec * 1000000 + end_time.tv_usec) - (start_time.tv_sec * 1000000 + start_time.tv_usec);
+		
+		printf("Completed in %uus\n\n\n", time);
+	}
+	
+	
+	/*----------------------------------------------------------------------*/
+	/*	CLEAN UP MEMORY MAPPING	*/
+	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
+		printf( "ERROR: munmap() failed...\n" );
+		close( fd );
+		return( 1 );
+	}
+
+}
--- VN_100/soft_spi.c
+++ VN_100/soft_spi.c
@@ -1,28 +1,101 @@
 /* soft_spi.c
  * 
- * Source file for c functions relating to the control system
+ * Source file for c functions relating to the VN-100's SPI
  *
  * Authors:	Zach Frazee
  * 
  *
  * Revions:
  * 2016-02-11	v0.1	Initial Revison
+ * 2016-03-12	v0.2	Removed unnessesary delays and modified to conform to new coding standard 
  *
  * */
  
 #include "soft_spi.h"
 
-//Memory Mapped Registers
+/*---------------------------------------------------------------------*/
+/* MEMORY MAPPED REGISTERS */
 void * spi_pio_data;
 void * spi_pio_datadir;
 
 int fd;
 void * virtual_base;
 
-//Initialized Variable
+/* Initialized Variable */
 char is_initialized = 0; 
 
-void spi_init(void * virtual_base ){
+
+
+/*---------------------------------------------------------------------*/
+/* PRIVATE FUNCTION PROTOTYPE DECLARATIONS */
+
+/*	SPI_Clock
+		Clocks the SPI line
+			Params:
+				state - 1 for clock high, 0 for clock low
+			Returns:
+				void 
+*/
+void SPI_Clock(char state);
+
+/*	SPI_MOSI
+		Changes the MOSI line
+			Params:
+				state - 1 for MOSI high, 0 for MOSI low
+			Returns:
+				void 
+*/
+void SPI_MOSI(char state);
+
+/* SPI_MISO
+		Gets a bit of data from the MISO
+			Params:
+				None
+			Returns:
+				uint8_t - Bit from MISO is the LSB
+*/
+uint8_t SPI_MISO();
+
+/* NOP_Delay
+		A precise delay that uses nops
+			Params:
+				counter - how many times NOP is called
+			returns:
+				void 
+*/
+void NOP_Delay(int count); void NOP();
+
+/*---------------------------------------------------------------------*/
+/* PRIVATE DEFINES */
+
+//#define USE_CLOCK_DELAY
+//#define USE_DATA_DELAY
+//#define USE_CLOCK_DELAY_NOP
+//#define USE_DATA_DELAY_NOP
+//#define USE_SAMPLING
+
+#define CLOCK_DELAY 5
+#define DATA_DELAY 5
+
+#define NOP_CLOCK_DELAY 100
+#define NOP_DATA_DELAY 100
+
+#define NUMBER_OF_SAMPLES 3
+#define SAMPLE_DELAY 1
+
+/*---------------------------------------------------------------------*/
+/* FUNCTION IMPLEMENTATIONS*/
+
+
+
+/* SPI_Init:
+		Initializes the SPI's memory mapping and data lines
+			Params:
+				virtual_base - base for memory mapping
+			Returns:
+				Void 
+*/
+void SPI_Init(void * virtual_base ){
 	spi_pio_data = virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + SPI_ADDR_PIO_DATA ) & ( unsigned long)( HW_REGS_MASK ) );
 	spi_pio_datadir = virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + SPI_ADDR_PIO_DATADIR ) & ( unsigned long)( HW_REGS_MASK ) );
 	
@@ -34,33 +107,100 @@ void spi_init(void * virtual_base ){
 	is_initialized = 1;
 }
 
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+/* SPI_Read_Write:
+		Preforms a SPI read write operation
+			Params:
+				data_out - The byte of data to be written
+			Returns:
+				uint8_t - The byte of data read 
+*/
 uint8_t SPI_Read_Write(uint8_t data_out){
-	//receive variable
+	/* receive variable */
 	uint8_t data_in = 0;
 	
-	//Byte loop 
+	/* Byte loop */
 	int i;
 	for(i = 0; i < 8; i++){
-		//pull clock low
+		/* pull clock low */
 		SPI_Clock(0);
-		usleep(5);
+		#ifdef USE_CLOCK_DELAY
+			usleep(CLOCK_DELAY);
+		#endif
 		
-		//Set MOSI
+		#ifdef USE_CLOCK_DELAY_NOP
+			NOP_Delay(NOP_CLOCK_DELAY);
+		#endif
+		
+		/* Set MOSI */
 		SPI_MOSI((data_out >> (7 - i)) & 1);
-		usleep(5);
 		
-		//Clock High
+		/* Wait for VN-100 to change MISO */
+		#ifdef USE_DATA_DELAY
+			usleep(DATA_DELAY);
+		#endif
+		
+		#ifdef USE_DATA_NOP
+			usleep(DATA_DELAY);
+		#endif
+		
+		/* Clock High */
 		SPI_Clock(1);
-		usleep(5);
 		
-		//Read MISO
-		data_in |= (SPI_MISO() << (7 - i));
-		usleep(5);
+		#ifdef USE_CLOCK_DELAY
+			usleep(CLOCK_DELAY);
+		#endif
+		
+		#ifdef USE_CLOCK_DELAY_NOP
+			NOP_Delay(NOP_CLOCK_DELAY);
+		#endif
+		
+		/* Read MISO */
+		#ifdef USE_SAMPLING
+			int sum_of_samples = 0;
+			int i;
+			for(i = 0; i < NUMBER_OF_SAMPLES; i++){
+				sum_of_samples += SPI_MISO();
+				NOP_Delay(SAMPLE_DELAY);
+			}
+			
+			float voting = (float) sum_of_samples / (float) NUMBER_OF_SAMPLES;
+			
+			if(voting > 0.5){
+				data_in |= (1 << (7 - i));
+			}else{
+				data_in |= (0 << (7 - i));
+			}	
+		#else
+			data_in |= (SPI_MISO() << (7 - i));
+		#endif
+		
+		#ifdef USE_CLOCK_DELAY
+			usleep(CLOCK_DELAY);
+		#endif
+		
+		#ifdef USE_CLOCK_DELAY_NOP
+			NOP_Delay(NOP_CLOCK_DELAY);
+		#endif
 	}
 	
 	return data_in;
 }
 
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+/*	SPI_Clock
+		Clocks the SPI line
+			Params:
+				state - 1 for clock high, 0 for clock low
+			Returns:
+				void 
+*/
 void SPI_Clock(char state){
 	if(state == 1){
 		*(uint32_t *)spi_pio_data |= 0x00000001;
@@ -69,6 +209,17 @@ void SPI_Clock(char state){
 	}
 }
 
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+/*	SPI_MOSI
+		Changes the MOSI line
+			Params:
+				state - 1 for MOSI high, 0 for MOSI low
+			Returns:
+				void 
+*/
 void SPI_MOSI(char state){
 	if(state == 1){
 		*(uint32_t *)spi_pio_data |= 0x00000008;
@@ -77,23 +228,67 @@ void SPI_MOSI(char state){
 	}
 }
 
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+/*	SPI_SS
+		Changes the SS line
+			Params:
+				state - 1 for SS high, 0 for SS low
+			Returns:
+				void 
+*/
 void SPI_SS(char state){
 	if(state == 1){
-		//Ensure that clock line is high
+		/* Ensure that clock line is high */
 		SPI_Clock(1);
 		
-		//set SS high
+		/* set SS high */
 		*(uint32_t *) spi_pio_data |= 0x00000002;
 	} else{
-		//Ensure that clock line is high
+		/* Ensure that clock line is high */
 		SPI_Clock(1);
 		
-		//set SS low
+		/* set SS low */
 		*(uint32_t *) spi_pio_data &= 0xFFFFFFFD;
 	}
 }
 
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+/* SPI_MISO
+		Gets a bit of data from the MISO
+			Params:
+				None
+			Returns:
+				uint8_t - Bit from MISO is the LSB
+*/
 uint8_t SPI_MISO(){
 	return (*(uint32_t *)spi_pio_data >> 2) & 1;
-	
 }
+
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+/* NOP_Delay
+		A precise delay that uses nops
+			Params:
+				counter - how many times NOP is called
+			returns:
+				void 
+*/
+void NOP_Delay(int count){
+	for(; count > 0; count--){
+		NOP();
+	}
+}
+
+void NOP(){
+	asm("nop;nop;nop;nop;nop;nop;nop;nop;nop;nop");
+} 
+
+/*---------------------------------------------------------------------------------------------*/
